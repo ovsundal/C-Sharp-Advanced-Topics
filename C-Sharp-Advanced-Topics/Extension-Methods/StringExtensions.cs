60dfe1b4ec0a46c68064f4a2b262694843eca165
Ove <osundal83@gmail.com>
2018-04-19 12:31:53 +0200

    add Extension-Methods

---
+++ C-Sharp-Advanced-Topics/Extension-Methods/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+//Added to system namespace to prevent import code bloat
+namespace System
+{
+    //Convention is to derive from original class if possible (ClassWithNewMethod : OriginalClass)
+    public static class StringExtensions
+    {
+        //Shorten a string to max 5 letters, followed by ... using a custom extension method
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+            }
+
+            if (numberOfWords == 0)
+            {
+                return "";
+            }
+
+            var words = str.Split(' ');
+            
+            if (words.Length <= numberOfWords)
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
--- C-Sharp-Advanced-Topics/Program.cs
+++ C-Sharp-Advanced-Topics/Program.cs
@@ -10,6 +10,10 @@ namespace C_Sharp_Advanced_Topics
     {
         static void Main(string[] args)
         {
+            //Extension-Methods
+            //string post = "This is supposed to be a very long blog post blah blah blah...";
+            //var shortenedPost = post.Shorten(5);
+            //Console.WriteLine(shortenedPost);
         }
     }
 }
