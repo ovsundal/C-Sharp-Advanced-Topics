70d944948fadbc3f0f2449ffba9100b301e2cbef
Ove <osundal83@gmail.com>
2018-04-20 16:18:20 +0200

    add NullableTypes

--- C-Sharp-Advanced-Topics/Program.cs
+++ C-Sharp-Advanced-Topics/Program.cs
@@ -11,43 +11,68 @@ namespace C_Sharp_Advanced_Topics
     {
         static void Main(string[] args)
         {
-            //---Extension-Methods---
+            ExtensionMethods();
+            Linq();
+            NullableTypes();
+        }
 
-            //string post = "This is supposed to be a very long blog post blah blah blah...";
-            //var shortenedPost = post.Shorten(5);
-            //Console.WriteLine(shortenedPost);
+        static void ExtensionMethods()
+        {
 
-            //---LINQ---
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost);
+        }
 
-            //var books = new BookRepository().GetBooks();
+        static void Linq()
+        {
+            var books = new BookRepository().GetBooks();
 
-            ////Query Operators
-            ////find books cheaper than $10 sorted by title, project to title
-            //var cheaperBooks = from b in books
-            //    where b.Price < 10
-            //    orderby b.Title
-            //    select b.Title;
+            //Query Operators
+            //find books cheaper than $10 sorted by title, project to title
+            var cheaperBooks = from b in books
+                               where b.Price < 10
+                               orderby b.Title
+                               select b.Title;
 
-            ////Extension Methods
-            ////same as above
-            //var cheapBooks = books
-            //                    .Where(b => b.Price < 10)
-            //                    .OrderBy(b => b.Title)
-            //                    .Select(b => b.Title);
+            //Extension Methods
+            //same as above
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)
+                                .OrderBy(b => b.Title)
+                                .Select(b => b.Title);
 
-            ////skip the first two objects, and return the next three
-            //var pagedBooks = books.Skip(2).Take(3);
+            //skip the first two objects, and return the next three
+            var pagedBooks = books.Skip(2).Take(3);
 
-            ////min-max-average-sum returns
-            //var maxPrice = books.Max(b => b.Price);
-            //var minPrice = books.Min(b => b.Price);
-            //var totalPrices = books.Sum(b => b.Price);
-            //var averagePrice = books.Average(b => b.Price);
+            //min-max-average-sum returns
+            var maxPrice = books.Max(b => b.Price);
+            var minPrice = books.Min(b => b.Price);
+            var totalPrices = books.Sum(b => b.Price);
+            var averagePrice = books.Average(b => b.Price);
 
+            Console.WriteLine(averagePrice);
+        }
 
+        static void NullableTypes()
+        {
+            DateTime? date = null;
 
+            //DateTime date2;
+            //if (date != null)
+            //{
+            //    date2 = date.GetValueOrDefault();
+            //}
+            //else
+            //{
+            //    date2 = DateTime.Today;
+            //}
 
+            //above statement written as ternary expression
+            DateTime date2 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
 
+            //Using Null coalescing operator - equal to above statement
+            DateTime date3 = date ?? DateTime.Today;
 
         }
     }
